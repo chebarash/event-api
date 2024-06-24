138d49b1bda1d814d1a69bf17dddf5adfd19ec9e
Shamsiddinkhuja Abdurakhimov <64980797+chebarash@users.noreply.github.com>
2024-06-24 18:04:42 +0500

    test

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 require(`dotenv`).config();
 const { Telegraf } = require(`telegraf`);
 const express = require(`express`);
+const axios = require("axios");
+const cors = require("cors");
 
-const { ADMIN_ID, TOKEN, VERCEL_URL, PORT } = process.env;
+const {
+  ADMIN_ID,
+  TOKEN,
+  VERCEL_URL,
+  PORT,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID,
+} = process.env;
 
 const bot = new Telegraf(TOKEN);
 const app = express();
 
 const adminId = parseInt(ADMIN_ID);
+
+bot.command(`test`, async (ctx) => {
+  await ctx.reply(
+    `https://myservice.example.com/auth?client_id=GOOGLE_CLIENT_ID&redirect_uri=REDIRECT_URI&state=STATE_STRING&response_type=token&user_locale=LOCALE`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: `test`,
+              url: `https://myservice.example.com/auth?client_id=GOOGLE_CLIENT_ID&redirect_uri=REDIRECT_URI&state=STATE_STRING&response_type=token&user_locale=LOCALE`,
+            },
+          ],
+        ],
+      },
+    }
+  );
+});
 
 bot.use(async (ctx) => {
   try {
@@ -49,6 +76,42 @@ bot.use(async (ctx) => {
       )}</code></pre>`,
       { parse_mode: `HTML` }
     );
+  }
+});
+
+app.use(cors());
+
+app.post("/auth", async (req, res) => {
+  try {
+    const code = req.headers.authorization;
+    console.log("Authorization Code:", code);
+
+    const response = await axios.post("https://oauth2.googleapis.com/token", {
+      code,
+      redirect_uri: "postmessage",
+      grant_type: "authorization_code",
+      client_id: GOOGLE_CLIENT_ID,
+      client_secret: GOOGLE_CLIENT_SECRET,
+      // redirect_uris: "https://event.chebarash.uz",
+    });
+    const accessToken = response.data.access_token;
+    console.log("Access Token:", accessToken);
+
+    const userResponse = await axios.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const userDetails = userResponse.data;
+    console.log("User Details:", userDetails);
+
+    res.status(200).json({ message: "Authentication successful" });
+  } catch (error) {
+    console.error("Error saving code:", error);
+    res.status(500).json({ message: "Failed to save code" });
   }
 });
 

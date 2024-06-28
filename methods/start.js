3fdaa3ad0f220de6d71491d8f843d990951fb35a
Shamsiddinkhuja Abdurakhimov <64980797+chebarash@users.noreply.github.com>
2024-06-28 17:51:50 +0500

    Update start.js

--- methods/start.js
+++ methods/start.js
@@ -2,7 +2,12 @@ const start = (ctx) =>
   ctx.reply(`welcome`, {
     reply_markup: {
       inline_keyboard: [
-        [{ text: `event`, web_app: { url: `https://event.chebarash.uz` } }],
+        [
+          {
+            text: `event`,
+            web_app: { url: `https://event.chebarash.uz/${ctx.user._id}` },
+          },
+        ],
       ],
     },
   });

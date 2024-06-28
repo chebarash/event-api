6a575efa4cabe8f983a3bed1ebe92b82f9bf2186
CheBarash <chebarashek@gmail.com>
2024-06-28 23:53:56 +0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 var ObjectId = require(`mongodb`).ObjectId;
-
 const users = require(`../models/user`);
 
-const userRoute = async ({ query: { _id } }, res) =>
-  res.json(await users.findOne(new ObjectId(_id)));
+const userRoute = async ({ query: { _id } }, res) => {
+  if (!_id) return res.status(500).json({ message: `_id is required` });
+  return res.json(await users.findOne(new ObjectId(_id)));
+};
 
 module.exports = userRoute;

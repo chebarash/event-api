b062fbd16d9c92363bcb25be588e0fd31faea416
CheBarash <chebarashek@gmail.com>
2024-07-01 11:34:34 +0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 const users = require(`../models/user`);
 
-const userRoute = async ({ query: { id } }, res) => {
-  if (!id) return res.status(500).json({ message: `id is required` });
-  return res.json(await users.findOne({ id: parseInt(id) }));
+const userRoute = async ({ headers: { authorization } }, res) => {
+  if (!authorization)
+    return res.status(500).json({ message: `User not found` });
+  const user = await users.findOne({ id: parseInt(authorization) });
+  if (!user) return res.status(500).json({ message: `User not found` });
+  return res.json(user);
 };
 
 module.exports = userRoute;

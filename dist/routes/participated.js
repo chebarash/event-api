270878ecdac090c039bf8712742b6433fc896086
CheBarash <chebarashek@gmail.com>
2025-01-03 03:05:10 +0500

    Participate

--- dist/routes/participated.js
+++ dist/routes/participated.js
@@ -12,45 +12,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const events_1 = __importDefault(require("../models/events"));
-const axios_1 = __importDefault(require("axios"));
 const participated = {
-    post: (_a, res_1) => __awaiter(void 0, [_a, res_1], void 0, function* ({ user, admin, body: { _id } }, res) {
-        if (!user)
-            return res.json([]);
-        if (!_id)
-            return res.json([]);
-        const event = yield events_1.default.findOneAndUpdate({ _id }, { $addToSet: { participated: user._id } }, { new: true, useFindAndModify: false })
+    post: (_a, res_1) => __awaiter(void 0, [_a, res_1], void 0, function* ({ user, body: { _id, participant } }, res) {
+        if (!user || !user || !participant)
+            return res.status(401).json({ message: "Unauthorized" });
+        const event = yield events_1.default.findOneAndUpdate({ _id }, { $addToSet: { participated: participant } }, { new: true, useFindAndModify: false })
             .populate([`author`, `registered`, `participated`])
             .exec();
         res.json(event);
-        if (event === null || event === void 0 ? void 0 : event.eventId) {
-            const startTime = new Date(event.date);
-            const endTime = new Date(startTime.getTime() + (event.duration || 0));
-            yield axios_1.default.put(`https://www.googleapis.com/calendar/v3/calendars/${admin.calendarId}/events/${event.eventId}`, {
-                summary: event.title,
-                location: event.venue,
-                description: event.description,
-                start: {
-                    dateTime: startTime.toISOString(),
-                },
-                end: {
-                    dateTime: endTime.toISOString(),
-                },
-                reminders: {
-                    useDefault: false,
-                    overrides: [{ method: "popup", minutes: 30 }],
-                },
-                attendees: event.registered.map(({ email }) => ({ email })),
-                guestsCanInviteOthers: false,
-                guestsCanSeeOtherGuests: false,
-                status: event.cancelled ? `cancelled` : `confirmed`,
-            }, {
-                headers: {
-                    Authorization: `Bearer ${admin.accessToken}`,
-                    "Content-Type": "application/json",
-                },
-            });
-        }
     }),
 };
 module.exports = participated;
--- src/routes/participated.ts
+++ src/routes/participated.ts
@@ -1,54 +1,21 @@
 import { RequestHandler } from "express";
 import { MethodsType } from "../types/types";
 import Events from "../models/events";
-import axios from "axios";
 
 const participated: {
   [name in MethodsType]?: RequestHandler;
 } = {
-  post: async ({ user, admin, body: { _id } }, res) => {
-    if (!user) return res.json([]);
-    if (!_id) return res.json([]);
+  post: async ({ user, body: { _id, participant } }, res) => {
+    if (!user || !user || !participant)
+      return res.status(401).json({ message: "Unauthorized" });
     const event = await Events.findOneAndUpdate(
       { _id },
-      { $addToSet: { participated: user._id } },
+      { $addToSet: { participated: participant } },
       { new: true, useFindAndModify: false }
     )
       .populate([`author`, `registered`, `participated`])
       .exec();
     res.json(event);
-    if (event?.eventId) {
-      const startTime = new Date(event.date);
-      const endTime = new Date(startTime.getTime() + (event.duration || 0));
-      await axios.put<{ id: string }>(
-        `https://www.googleapis.com/calendar/v3/calendars/${admin.calendarId}/events/${event.eventId}`,
-        {
-          summary: event.title,
-          location: event.venue,
-          description: event.description,
-          start: {
-            dateTime: startTime.toISOString(),
-          },
-          end: {
-            dateTime: endTime.toISOString(),
-          },
-          reminders: {
-            useDefault: false,
-            overrides: [{ method: "popup", minutes: 30 }],
-          },
-          attendees: event.registered.map(({ email }) => ({ email })),
-          guestsCanInviteOthers: false,
-          guestsCanSeeOtherGuests: false,
-          status: event.cancelled ? `cancelled` : `confirmed`,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${admin.accessToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
   },
 };
 

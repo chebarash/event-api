f7b9cc75b4196c583678a0bfe55665508cef2e59
CheBarash <chebarashek@gmail.com>
2025-01-08 20:11:40 +0500

    Bug fix

--- dist/methods/login.js
+++ dist/methods/login.js
@@ -2,7 +2,7 @@
 const { GOOGLE_AUTH_URL } = process.env;
 const login = (ctx, option) => {
     var _a;
-    return ctx.telegram.sendMessage(ctx.from.id, `Welcome to the bot where you can become part of the university community.\n\nTo continue, you must <b>log in using your student email</b>.`, {
+    return ctx.telegram.sendMessage(ctx.from.id, `Welcome to the bot where you can become part of the university community.\n\nTo continue, you must <b>log in using your your New Uzbekistan University email address</b>  <code>(@newuu.uz)</code>.\n\n<b>Need Help?</b> Contact <a href="https://t.me/m/_mZ-DLfsNzZi">@chebarash</a>`, {
         reply_markup: {
             inline_keyboard: [
                 [
--- src/methods/login.ts
+++ src/methods/login.ts
@@ -5,7 +5,7 @@ const { GOOGLE_AUTH_URL } = process.env;
 const login = (ctx: MyContext, option?: string) =>
   ctx.telegram.sendMessage(
     ctx.from!.id,
-    `Welcome to the bot where you can become part of the university community.\n\nTo continue, you must <b>log in using your student email</b>.`,
+    `Welcome to the bot where you can become part of the university community.\n\nTo continue, you must <b>log in using your your New Uzbekistan University email address</b>  <code>(@newuu.uz)</code>.\n\n<b>Need Help?</b> Contact <a href="https://t.me/m/_mZ-DLfsNzZi">@chebarash</a>`,
     {
       reply_markup: {
         inline_keyboard: [
